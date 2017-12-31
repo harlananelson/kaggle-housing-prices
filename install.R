08e8e9ad34eb9d73135df2968033a39916a2638f
harlananelson <harlananelson@gmail.com>
2017-12-31 12:41:23 -0600

    commit

--- install.R
+++ install.R
@@ -1,1 +1,16 @@
 install.packages('corrplot')
+# make sure you have these packages of using Ubuntu
+# sudo apt-get install curl
+# sudo apt-get install libcurl4-openssl-dev
+# libxml2-dev, libcurl4-openssl-dev, and libssl-dev, 
+install.packages('devtools')
+install.packages('httr')
+install.packages('tidyverse')
+install.packages('haven')
+install.packages('httr')
+install.packages('readr')
+install.packages('rvest')
+install.packages('xml2')
+install.packages('Hmisc')
+install.packages('shiny')
+install.packages('miniUI')
